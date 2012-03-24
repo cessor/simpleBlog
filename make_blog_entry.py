604d12f1b4668d803101099db63b9c4ba787692f
eeemsi <eeemsi@gmail.com>
2012-03-24 14:11:43 +0100

    reimplemented the write to file stuff in a nicer way

--- make_blog_entry.py
+++ make_blog_entry.py
@@ -1,9 +1,19 @@
 from __future__ import unicode_literals
-import json
 from glob import glob
+import json
+
+html="<!DOCTYPE html>" + \
+     "<html>" + \
+     "<head>" + \
+     "<meta charset='utf-8'>" + \
+     "<title>simpleBlog</title>" + \
+     "</head>" + \
+     "<link rel='stylesheet' href='simple.css' type='text/css'>" + \
+     "<body>\n"
 
 def read_json():
   storage = []
+
   for i in glob('./*.json'):
     f = open(i).read()
     data = json.loads(f)
@@ -12,20 +22,32 @@ def read_json():
     timestamp = data["timestamp"]
     #print title, text, timestamp
     storage.append((title, text, timestamp))
+
   storage.sort(key=lambda timestamp: timestamp[2])
   #print storage
   return storage
 
-def write_html():
+def create_html(html):
   data = read_json()
   #print data
-  f = open('./index.html', 'w')
-  f.write("<html><body>\n")
 
-  for i in data:
-    f.write("<h1>{0}</h1><p>{1}<br /><br />{2}</p>\n".format(*i))
+  for index, item in enumerate(data):
+    html+="<h1>"
+    for i in item[0]:
+        html+=i
+    html+="</h1>"
 
-  f.write("</body></html>")
-  f.close()
+    html+="<p>"
+    for i in item[1]:
+        html+=i
 
-write_html()
+    for i in item[2]:
+        html+="<br />"+i
+    html+="</p>"
+
+  html += "</body></html>"
+  return html
+
+f = open('./index.html', 'w')
+f.write(create_html(html))
+f.close()
