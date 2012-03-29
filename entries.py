31cbea0f1acd44618d1e8ef033cb401b7ae372d9
cessor <Johannes Hofmeister@nossi-home.(none)>
2012-03-29 13:45:17 +0200

    Moved the template and entry management into separate files. After all they are different concerns

---
+++ entries.py
@@ -0,0 +1,25 @@
+from json import load
+from glob import glob
+from convert_date import convert_date
+
+class Entries:
+  def read(self):
+    entries = []
+    for path in glob('./entries/*.json'):
+      object = self.read_json_object(path)
+      object["date"] = convert_date(object["timestamp"])
+      entries.append(object)
+    return entries
+  
+  def read_json_object(self, path): 
+    return load(open(path, 'r'))
+  
+  def sort_by_date(self, entries):
+    by_date = lambda object: object["timestamp"]
+    entries.sort(key=by_date, reverse=True)
+
+  # iterator pattern: http://en.wikipedia.org/wiki/Iterator
+  def __iter__(self): 
+    entries = self.read()
+    self.sort_by_date(entries)
+    return entries.__iter__()
--- make_blog_entry.py
+++ make_blog_entry.py
@@ -1,47 +1,23 @@
 from __future__ import unicode_literals
-from glob import glob
-from sys import argv
-from convert_date import convert_date
-import json
+from template import Template
+from entries import Entries
+
 blog_name = ".::a small blog"
-debug = True
-
-def read_json_object(path): 
-  return json.load(open(path, 'r'))
-
-def read_json():
-  entries = []
-  for path in glob('./*.json'):
-    object = read_json_object(path)
-    object["date"] = convert_date(object["timestamp"])
-    entries.append(object)
-  by_date = lambda object: object["date"]
-  entries.sort(key=by_date, reverse=True)
-  return entries
   
-def read_all(path):
-  content = ""
-  with open(path, 'r') as file:
-    content = file.read()
-  return content
+def create_page(): 
+  content = create_markup_for_all(Entries())
+  view_data = { "blog_name" : blog_name, "content" : content }
+  return Template('templates/page.template', view_data)
   
-def create_html():
-  page = read_all("page.template")
-  entries = read_json()
-  content = ""
-  for entry in entries:
-    content += create_entry(entry) # this is bad, use stringio instead
-  view_data = { "blog_name" : blog_name, "text" : content }
-  return template(page, view_data)
-
-def create_entry(entry): # this is called per loop ant therefore inefficient, cache it!
-  text = read_all("entry.template")
-  return template(text, entry)
-  
-def template(text, map): 
-  for field in map:
-    text = text.replace("$%s$" % field, map[field]) # also slow
-  return text
+def create_markup_for_all(entries):
+  markup = ""
+  for entry in Entries():   
+    markup += str(Template("templates/entry.template", entry)) 
+  return markup
 
 if __name__ == "__main__":
-  print create_html()
+  print create_page()
+  
+# Remarks: 
+   # Calling Template per loop does I/O and should be cached instead!
+   # The String concatenation is also slow
