c1e5966b07557dcb569e3920e6755e57fe96d5f8
eeemsi <eeemsi@gmail.com>
2012-03-29 21:50:01 +0200

    some fixes and clean ups

--- entries.py
+++ entries.py
@@ -6,20 +6,20 @@ class Entries:
   def read(self):
     entries = []
     for path in glob('./entries/*.json'):
-      object = self.read_json_object(path)
+      object = read_json_object(path)
       object["date"] = convert_date(object["timestamp"])
       entries.append(object)
     return entries
-  
-  def read_json_object(self, path): 
-    return load(open(path, 'r'))
-  
+
   def sort_by_date(self, entries):
     by_date = lambda object: object["timestamp"]
     entries.sort(key=by_date, reverse=True)
 
   # iterator pattern: http://en.wikipedia.org/wiki/Iterator
-  def __iter__(self): 
+  def __iter__(self):
     entries = self.read()
     self.sort_by_date(entries)
     return entries.__iter__()
+
+def read_json_object(path):
+  return load(open(path, 'r'))
--- test_blog.py
+++ test_blog.py
@@ -9,14 +9,14 @@ class BlogTests(unittest.TestCase):
 		self.test_file_path = 'test.json'
 		with open(self.test_file_path,'w') as f:
 			f.write('{ "test" : "this is a test"}')
-			
+
 	def test_json_can_read_a_file_directly(self):
-		object = read_json_object(self.test_file_path)
-		self.assertEqual(object["test"], "this is a test")
-	
+	    object = read_json_object(self.test_file_path)
+	    self.assertEqual(object["test"], "this is a test")
+
 	@classmethod
 	def tearDownClass(self):
 		os.remove(self.test_file_path)
-		
+
 if __name__ == '__main__':
 	unittest.main()
