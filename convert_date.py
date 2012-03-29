3a4694262ffd76648f77bada41680b28dae3be31
eeemsi <eeemsi@gmail.com>
2012-03-29 22:01:15 +0200

    changed output of date

--- convert_date.py
+++ convert_date.py
@@ -4,7 +4,7 @@ def parse_date(date):
   return datetime.strptime(date, "%Y-%m-%d")
 
 def reformat_date(date):
-  return datetime.strftime(date, "%d.%m.%Y")
+  return datetime.strftime(date, "%A, %d. %B %Y")
 
 def convert_date(date):
   return reformat_date(parse_date(date))
